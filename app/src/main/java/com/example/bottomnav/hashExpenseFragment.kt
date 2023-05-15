4f5d3ed562d123099680f2c08f8ab69d6f97975b
Hashan Perera <hashperera.v@gmail.com>
2023-05-15 10:17:30 +0530

    Commit by Hashan Test cases and some updates

--- app/src/main/java/com/example/bottomnav/hashExpenseFragment.kt
+++ app/src/main/java/com/example/bottomnav/hashExpenseFragment.kt
@@ -1,59 +1,49 @@
 package com.example.bottomnav
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [hashExpenseFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class hashExpenseFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hash_expense, container, false)
+        var ret =  inflater.inflate(R.layout.fragment_hash_expense, container, false)
+
+        pieChart = ret.findViewById(R.id.pieChartGraph1)
+        val list: ArrayList<PieEntry> = ArrayList()
+
+        list.add(PieEntry(50000f, "Lease"))
+        list.add(PieEntry(30000f, "Food"))
+        list.add(PieEntry(10000f, "Travel"))
+        list.add(PieEntry(10000f, "Medicine"))
+        list.add(PieEntry(20000f, "House Rent"))
+
+        val pieDataSet = PieDataSet(list, "List")
+
+        pieDataSet.setColors(ColorTemplate.MATERIAL_COLORS,255)
+        pieDataSet.valueTextSize=25f
+        pieDataSet.valueTextColor= Color.BLACK
+
+        val pieData = PieData(pieDataSet)
+        pieChart.data = pieData
+        pieChart.description.text = "Pie Chart"
+        pieChart.centerText = "List"
+        pieChart.animateY(2000)
+
+        return ret
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment hashExpenseFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            hashExpenseFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
